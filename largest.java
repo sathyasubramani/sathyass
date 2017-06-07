65db9fded4e115004ab2a6eb5aa36d5a40fb64a4
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-06-07 12:27:58 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+class Largest
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("the largest number");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if((a>b)&&(a>c))
+{
+System.out.println("a is the largest number");
+}
+else if((b>a)&&(b>c))
+{
+System.out.println("b is the largest number");
+}
+else
+{
+System.out.println("c is the largest number");
+}
+sc.close();
+}
+}
