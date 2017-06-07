ed26bc63d2e49df23a2a80df91068c9f84254912
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-06-07 15:14:32 +0530

    Create SumOfNum.java

---
+++ SumOfNum.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class SumOfNum
+{
+public static void main(String args[])
+{
+int sum=0;
+int n=10;
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out println("the sum of N nu8mbers"+sum);
+}
+}
