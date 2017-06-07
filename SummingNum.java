d6196057bdf239c7ba68dbe042f49a92e196b942
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-06-07 15:48:18 +0530

    Create SummingNum.java

---
+++ SummingNum.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class SummingNum
+{
+public static void main(String args[])
+{
+int sum=0;
+int n=10;
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println("the sum of numbers"+sum)
+}
+}
