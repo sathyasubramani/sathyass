33a7b5b651945de08ad8e41ce9f43cb8fb5d3722
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-06-29 10:48:48 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,17 @@
+package sir;
+
+import java.util.Scanner;
+
 public class Factorial {
-	 public static void main(String args[]){  
-		  int i,fact=1;  
-		  int number=7;   
-		  for(i=1;i<=number;i++){    
-		      fact=fact*i;    
-		  }    
-		  System.out.println("Factorial of "+number+" is: "+fact);    
-		 }  
+
+	public static void main(String[] args) {
+	int fact=1;
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		for(int i=1;i<=s;i++){
+			 fact=fact*i;
 		}
+System.out.println("fact"+fact);
+	}
+
+}
