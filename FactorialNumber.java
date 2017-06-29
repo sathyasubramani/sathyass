896388376fcf95a117050b608df1bebaee27cd89
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-06-29 11:53:55 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,17 @@
+package sir;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+	int fact=1;
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		for(int i=1;i<=s;i++){
+			 fact=fact*i;
+		}
+System.out.println("fact"+fact);
+	}
+
+}
