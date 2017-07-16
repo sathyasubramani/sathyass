e3a399fc17efe5255c9d5f851a2c11d0db1cef07
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-07-16 15:28:04 +0530

    Add files via upload

---
+++ ConcatTwoString.java
@@ -0,0 +1,15 @@
+package sssss;
+
+import java.util.Scanner;
+
+public class ConcatTwoString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the Strings");
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		System.out.println(a+b);
+	}
+
+}
