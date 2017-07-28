1b7214ac5f1e39d6a656ae360b7a0040cba53453
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-07-28 14:35:19 +0530

    Add files via upload

---
+++ Monday.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Monday {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the days");
+		String s=sc.nextLine();
+		if(s.equals("sunday")){
+			System.out.println("false");
+			}
+		else
+System.out.println("true");
+	}
+
+}
